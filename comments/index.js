b255b287fd0b312869e2fe530dd4761960e062a0
= <=>
2025-04-28 08:34:27 +0530

    implement basic comments service.

---
+++ comments/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5001;
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    const postComments = commentsByPostId[postId] || [];
+
+    res.send(postComments);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    try {
+        const { content } = req.body;
+        const postId = req.params.id;
+        const commentId = randomBytes(4).toString('hex');
+    
+        const postComments = commentsByPostId[postId] || [];
+        postComments.push({ id: commentId, content });
+        commentsByPostId[postId] = postComments;
+    
+        res.send(postComments);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
