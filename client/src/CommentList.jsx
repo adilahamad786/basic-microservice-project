0a3f5db1cf129902fdd1de75167d3cd0a2ba0f10
= <=>
2025-05-03 23:04:37 +0530

    show comments according to thier status.

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -4,11 +4,18 @@ function CommentList({comments}) {
             <h6>Comments</h6>
             <ul>
                 {
-                    comments.map((comment) => (
-                        <li key={comment.id}>
-                            {comment.content}
-                        </li>
-                    ))
+                    comments.map((comment) => {
+                        let content = comment.content;
+                        if (comment.status === 'approved') {
+                            content = comment.content;
+                        } else if (comment.status === 'pending') {
+                            content = 'This comment is awaiting moderation.';
+                        } else if (comment.status === 'rejected') {
+                            content = 'This comment has been rejected.';
+                        }
+                        
+                        return <li key={comment.id}>{content}</li>;
+                    })
                 }
             </ul>
         </div>
